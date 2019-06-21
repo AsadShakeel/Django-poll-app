5734b1b5672f4daac233f777cdfd99df1b6c5d9e
Technosoft <newtonx@techno-soft.com>
2019-06-21 11:39:08 +0500

    detail page with http404

--- polls/views.py
+++ polls/views.py
@@ -14,7 +14,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {question})
 
 
 def results(request, question_id):
